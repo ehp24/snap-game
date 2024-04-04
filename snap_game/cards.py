f184a2de9bf5c813c7a9d6952c45b19af8f39421
ehp24 <ep590@cam.ac.uk>
2024-04-04 22:10:47 +0100

    Complete test_deal_cards unit test

--- snap_game/cards.py
+++ snap_game/cards.py
@@ -23,18 +23,50 @@ class Deck:
         self.cards = [Card(suit, val) for suit in SUITS for val in RANK]
         
     def shuffle(self):
-        return random.shuffle(self.cards)
+        random.shuffle(self.cards)
     
     def get_cards(self):
         for card in self.cards:
             print(card)
+    
+    def draw(self):
+        return self.cards.pop()
         
         
 class Player:
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
+        self.hand = [] # players always start with empty hands
         
+        
+class Game:
+    def __init__(self, players: list[Player], game_deck: Deck) -> None:
+        self.players = players
+        self.game_deck = game_deck
 
+    def shuffle_deck(self):
+        pass
+        
+    def deal_cards(self):
+        # draw cards one by one and append to each Player's hand until deck is empty
+        while len(self.game_deck.cards)>0:
+            for player in self.players:
+                if len(self.game_deck.cards)>0:
+                    drawn_card = self.game_deck.draw()
+                    player.hand.append(drawn_card) 
+
+
+# deck = Deck()
+# players = [Player("Ellie"),Player("Jane")]
+# game = Game(players,deck)
+# for p in game.players:
+#     print(p.name)
+#     print(len(p.hand))
+    
+# game.deal_cards()
+# for p in game.players:
+#     print(p.name)
+#     print(len(p.hand))
 
 
 
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,4 +1,4 @@
-from snap_game.cards import Card, Deck, Player
+from snap_game.cards import Card, Deck, Player, Game
 import pytest
 
 SUITS = ["Hearts", "Diamonds", "Spades", "Clubs"]
@@ -40,6 +40,17 @@ def test_deck_shuffle(deck):
     shuffled_2 = deck.cards.copy()
     assert shuffled_2 != shuffled_1
     
+def test_deck_draw_card(deck):
+    initial_count = len(deck.cards)
+    # card_on_top = deck.cards[-1]
+    drawn_card = deck.draw()
+    assert type(drawn_card) == Card #can use isinstance() to check for subclasses also
+    # assert drawn_card == card_on_top 
+    assert drawn_card not in deck.cards
+    assert len(deck.cards) == initial_count -1
+    
+    
+    
     
 @pytest.fixture
 def player1():
@@ -47,3 +58,46 @@ def player1():
 
 def test_player_init(player1):
     assert player1.name == "Jane"
+    assert player1.hand == []
+    
+    
+
+@pytest.fixture
+def players():
+    return [Player("Jane"),Player("Jack"),Player("Jill")]
+
+
+def test_game_init(players, deck):
+    game = Game(players,deck)
+    assert game.players == players
+    assert game.game_deck == deck
+    
+    
+@pytest.fixture
+def game(players,deck):
+    return Game(players,deck)
+
+def test_deal_cards(game):
+    total_cards = len(game.game_deck.cards)
+    num_players = len(game.players)
+    
+    # deal cards
+    game.deal_cards()
+    
+    player_hands = [len(p.hand) for p in game.players]
+    evenly_cards = total_cards // num_players
+    leftover_cards = total_cards % num_players
+    test_hands = [evenly_cards]*num_players
+    for i in range(leftover_cards):
+        test_hands[i]+=1
+        
+    assert len(game.game_deck.cards) == 0
+    assert player_hands == test_hands
+    
+    
+    
+    
+    
+    
+    
+    
